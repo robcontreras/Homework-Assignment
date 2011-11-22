7aea07c9e0d61f70725e0ce34ee800c2ff7904c7
Rob Contreras <robcontreras@esmitv.com>
2011-11-22 09:25:06 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EsMitv::Application.config.secret_token = '06ad74d29c0f2582d9e92f8120fec2eae7873719ffb9c359729a803e85b4a2fc3e16648359ca3fc9fd6c23e5feae1bf1a9c5545ef0fa052f2963170d6876e021'
